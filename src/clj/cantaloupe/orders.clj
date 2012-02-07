3ad52512ce2eabc43a9622110164f3e48d0e24a1
François Le Lay <mfworx@gmail.com>
2012-02-07 22:27:07 +0100

    deleted useless comments

--- src/clj/cantaloupe/orders.clj
+++ src/clj/cantaloupe/orders.clj
@@ -3,12 +3,11 @@
   (:require [cascalog [workflow :as w] [ops :as c] [vars :as v]])
   (:use [cantaloupe.thrift])
   (:import [net.cantaloupe.thrift Order Status]))
-;  (:import [backtype.hadoop ThriftSerialization]))
 
 (defn- deserialize-order [d] (from-compact-thrift d (Order.)))
 (defn- getAmount [^Order t] (.getAmount t))
 
-;; Generate 10 serialized random orders
+;; Generate 100 serialized random orders
 
 (def orders
     (vec (for [i (range 100 200)]
@@ -19,8 +18,6 @@
             (.setAmount (+ 9.9 i))
             (.setStatus (cond (= r 0) Status/PENDING (= r 1) Status/DESPATCHED)))))))))
 
-;;(println orders)
-
 ;; Dump orders
 (defn dump-orders []
   (?<- (hfs-seqfile "/tmp/thrift-orders" :sinkmode :replace)
@@ -28,7 +25,6 @@
         (orders ?o)))
 
 ;; Reload orders and display amount
-
 (def orders-src (hfs-seqfile "/tmp/thrift-orders"))
 
 (defn load-orders []
@@ -37,6 +33,4 @@
         (orders-src ?s)
         (deserialize-order ?s :> ?d)
         (getAmount ?d :> ?a)))
-
-;;      (* 2 ?b :> ?c))
 
